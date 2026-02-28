7c809a196336b8dde4748a2496735e1f03cf9599
Nobuyuki Iwanaga <ufcpp@live.jp>
2026-02-28 22:30:26 +0900

    Refactor code generation with helper methods
    Modularize source code generation by introducing GenerateMemberDeclaration and GenerateContent helper methods. Move formatting and modifier logic into these helpers, improving readability and maintainability. Add newLine helper for line breaks and streamline main loop to use the new structure.

--- Generators/AttributeTemplates/Application/GenerationInfo.cs
+++ Generators/AttributeTemplates/Application/GenerationInfo.cs
@@ -13,122 +13,12 @@ internal record GenerationInfo(string Attribute, TemplateHierarchy Templates, Pa
 
         foreach (var (node, mt) in Templates)
         {
-            if (node is CompilationUnitSyntax)
-            {
-            }
-            else if (node is NamespaceDeclarationSyntax n)
-            {
-                s.Append($$"""
-                        namespace {{n.Name}} {
-
-                        """);
-            }
-            else if (node is TypeDeclarationSyntax type)
-            {
-                appendModifiers(type.Modifiers);
-
-                var keyword = type.Kind() switch
-                {
-                    SyntaxKind.ClassDeclaration => "class",
-                    SyntaxKind.StructDeclaration => "struct",
-                    SyntaxKind.RecordDeclaration => "record",
-                    SyntaxKind.RecordStructDeclaration => "record struct",
-                    _ => null, //todo: error
-                };
-
-                s.Append($$"""
-                        {{keyword}} {{type.Identifier.ValueText}} {
-
-                        """);
-            }
-            else if (node is PropertyDeclarationSyntax p)
-            {
-                appendModifiers(p.Modifiers);
-                s.Append($$"""
-                        {{p.Type}} {{p.Identifier.ValueText}} {
-
-                        """);
-            }
-            else if (node is MethodDeclarationSyntax m)
-            {
-                appendModifiers(m.Modifiers);
-                s.Append($$"""
-                        {{m.ReturnType}} {{m.Identifier.ValueText}}(
-                        """);
-
-                var first = true;
-                foreach (var mp in new TemplateHierarchy.Parameters(m.ParameterList))
-                {
-                    if (first) first = false;
-                    else s.Append(", ");
-                    s.Append($"{mp.Type} {mp.Name}");
-                }
-
-                s.Append("""
-                        ) {
-
-                        """);
-            }
-            else { } // todo: error? never reachable?
+            GenerateMemberDeclaration(s, node);
 
             // apply member template
             if (mt is null) continue;
 
-            if (mt.Constant is { } constValue)
-            {
-                s.Append(constValue);
-            }
-            else if (mt.Identifier is { } id)
-            {
-                var value = Params[id];
-                s.Append(value); // todo: take culture
-            }
-            else if (mt.Interpolation is { } i)
-            {
-                foreach (var c in i.Contents)
-                {
-                    if (c.Text is { } text)
-                    {
-                        s.Append(text);
-                    }
-                    else if (c.ConstantValue is { } c1)
-                    {
-                        s.AppendFormat(null, formatString(c.Alignment, c.Format), c1); // todo: take culture
-                    }
-                    else if (c.Identifier is { } id1)
-                    {
-                        if (id1 == Intrinsic.Type)
-                        {
-                            if (c.Alignment is { } a)
-                            {
-                                s.Append(Templates.MethodParameters[a].Type);
-                            }
-                            else
-                            {
-                                s.Append(Templates.GetNode(c.Level).Type);
-                            }
-                        }
-                        else if (id1 == Intrinsic.Name)
-                        {
-                            if (c.Alignment is { } a)
-                            {
-                                s.Append(Templates.MethodParameters[a].Name);
-                            }
-                            else
-                            {
-                                s.Append(Templates.GetNode(c.Level).Name);
-                            }
-                        }
-                        else
-                        {
-                            // must be distinguish "not found" and "found null"
-                            var value = Params[id1];
-                            s.AppendFormat(null, formatString(c.Alignment, c.Format), value); // todo: take culture
-                        }
-                    }
-                }
-            }
-            else { }
+            GenerateContent(s, mt);
             newLine();
         }
 
@@ -142,14 +32,11 @@ internal record GenerationInfo(string Attribute, TemplateHierarchy Templates, Pa
 ");
         }
 
-        static string formatString(int? alignment, string? format) => (alignment, format) switch
-        {
-            ({ } x, null) => $"{{0:{x}}}",
-            (null, { } x) => $"{{0,{x}}}",
-            ({ } x, { } y) => $"{{0,{x}:{y}}}",
-            _ => "{0}",
-        };
+        return s.ToString();
+    }
 
+    private static void GenerateMemberDeclaration(StringBuilder s, SyntaxNode node)
+    {
         void appendModifiers(SyntaxTokenList modifiers)
         {
             foreach (var token in modifiers)
@@ -159,6 +46,133 @@ internal record GenerationInfo(string Attribute, TemplateHierarchy Templates, Pa
             }
         }
 
-        return s.ToString();
+        if (node is CompilationUnitSyntax)
+        {
+        }
+        else if (node is NamespaceDeclarationSyntax n)
+        {
+            s.Append($$"""
+                        namespace {{n.Name}} {
+
+                        """);
+        }
+        else if (node is TypeDeclarationSyntax type)
+        {
+            appendModifiers(type.Modifiers);
+
+            var keyword = type.Kind() switch
+            {
+                SyntaxKind.ClassDeclaration => "class",
+                SyntaxKind.StructDeclaration => "struct",
+                SyntaxKind.RecordDeclaration => "record",
+                SyntaxKind.RecordStructDeclaration => "record struct",
+                _ => null, //todo: error
+            };
+
+            s.Append($$"""
+                        {{keyword}} {{type.Identifier.ValueText}} {
+
+                        """);
+        }
+        else if (node is PropertyDeclarationSyntax p)
+        {
+            appendModifiers(p.Modifiers);
+            s.Append($$"""
+                        {{p.Type}} {{p.Identifier.ValueText}} {
+
+                        """);
+        }
+        else if (node is MethodDeclarationSyntax m)
+        {
+            appendModifiers(m.Modifiers);
+            s.Append($$"""
+                        {{m.ReturnType}} {{m.Identifier.ValueText}}(
+                        """);
+
+            var first = true;
+            foreach (var mp in new TemplateHierarchy.Parameters(m.ParameterList))
+            {
+                if (first) first = false;
+                else s.Append(", ");
+                s.Append($"{mp.Type} {mp.Name}");
+            }
+
+            s.Append("""
+                        ) {
+
+                        """);
+        }
+        else { } // todo: error? never reachable?
+    }
+
+    private void GenerateContent(StringBuilder s, Templates.MemberTemplate mt)
+    {
+        void appendFormat(object? value, int? alignment = null, string? format = null)
+        {
+            var formatString = (alignment, format) switch
+            {
+                ({ } x, null) => $"{{0:{x}}}",
+                (null, { } x) => $"{{0,{x}}}",
+                ({ } x, { } y) => $"{{0,{x}:{y}}}",
+                _ => "{0}",
+            };
+            s.AppendFormat(null, formatString, value); // todo: take culture
+        }
+
+        if (mt.Constant is { } constValue)
+        {
+            s.Append(constValue);
+        }
+        else if (mt.Identifier is { } id)
+        {
+            var value = Params[id];
+            appendFormat(value);
+        }
+        else if (mt.Interpolation is { } i)
+        {
+            foreach (var c in i.Contents)
+            {
+                if (c.Text is { } text)
+                {
+                    s.Append(text);
+                }
+                else if (c.ConstantValue is { } c1)
+                {
+                    appendFormat(c1, c.Alignment, c.Format);
+                }
+                else if (c.Identifier is { } id1)
+                {
+                    if (id1 == Intrinsic.Type)
+                    {
+                        if (c.Alignment is { } a)
+                        {
+                            s.Append(Templates.MethodParameters[a].Type);
+                        }
+                        else
+                        {
+                            s.Append(Templates.GetNode(c.Level).Type);
+                        }
+                    }
+                    else if (id1 == Intrinsic.Name)
+                    {
+                        if (c.Alignment is { } a)
+                        {
+                            s.Append(Templates.MethodParameters[a].Name);
+                        }
+                        else
+                        {
+                            s.Append(Templates.GetNode(c.Level).Name);
+                        }
+                    }
+                    else
+                    {
+                        // must be distinguish "not found" and "found null"
+                        var value = Params[id1];
+                        appendFormat(value, c.Alignment, c.Format);
+                    }
+                }
+            }
+        }
+        else { }
     }
 }
