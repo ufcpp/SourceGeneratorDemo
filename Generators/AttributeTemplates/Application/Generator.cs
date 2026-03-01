221d6dc9d366514bd160ca0cfe271a9381b3235b
Nobuyuki Iwanaga <ufcpp@live.jp>
2026-03-02 00:59:59 +0900

    Refactor Generator: extract ExpressionEvaluator & MemberFormatter

--- Generators/AttributeTemplates/Application/Generator.cs
+++ Generators/AttributeTemplates/Application/Generator.cs
@@ -1,8 +1,6 @@
 using Generators.AttributeTemplates.Targets;
 using Generators.AttributeTemplates.Templates;
 using System.Text;
-using M = Generators.AttributeTemplates.Targets.MemberItem;
-using E = Generators.AttributeTemplates.Templates.MemberExpression;
 
 namespace Generators.AttributeTemplates.Application;
 
@@ -17,7 +15,7 @@ internal static class Generator
         var contents = Apply(target, templates);
         foreach (var (node, code) in target.Member.Zip(contents, (x, y) => (x, y)).Reverse())
         {
-            GenerateMemberDeclaration(s, node);
+            MemberFormatter.AppendDeclarationLine(s, node);
             s.Append(code);
         }
 
@@ -27,67 +25,6 @@ internal static class Generator
 ");
 
         return s.ToString();
-    }
-
-    private static void GenerateMemberDeclaration(StringBuilder s, M node)
-    {
-        void appendModifiers(string[] modifiers)
-        {
-            foreach (var token in modifiers)
-            {
-                s.Append(token);
-                s.Append(' ');
-            }
-        }
-
-        if (node is M.Root)
-        {
-        }
-        else if (node is M.Namespace n)
-        {
-            s.Append($$"""
-                        namespace {{n.Name}} {
-
-                        """);
-        }
-        else if (node is M.TypeDeclaration type)
-        {
-            appendModifiers(type.Modifiers);
-
-            s.Append($$"""
-                        {{type.Keyword}} {{type.Name}} {
-
-                        """);
-        }
-        else if (node is M.Property p)
-        {
-            appendModifiers(p.Modifiers);
-            s.Append($$"""
-                        {{p.Type}} {{p.Name}} {
-
-                        """);
-        }
-        else if (node is M.Method m)
-        {
-            appendModifiers(m.Modifiers);
-            s.Append($$"""
-                        {{m.Type}} {{m.Name}}(
-                        """);
-
-            var first = true;
-            foreach (var mp in m.Parameters)
-            {
-                if (first) first = false;
-                else s.Append(", ");
-                s.Append($"{mp.Type} {mp.Name}");
-            }
-
-            s.Append("""
-                        ) {
-
-                        """);
-        }
-        else { } // todo: error? never reachable?
     }
 
     private static string?[] Apply(TemplateTarget target, Dictionary<string, TemplateDefinition> templates)
@@ -102,7 +39,7 @@ internal static class Generator
 
                 foreach (var mt in template.Templates)
                 {
-                    var c = Evaluate(mt.Expression, target.Member, map);
+                    var c = ExpressionEvaluator.Evaluate(mt.Expression, target.Member, map);
                     var i = target.Member.GetIndex(mt.Level);
                     contents[i] += c + @"
 ";
@@ -111,51 +48,5 @@ internal static class Generator
         }
 
         return contents;
-    }
-
-    private static object? Evaluate(E ex, MemberHierarchy member, ParameterMap map)
-    {
-        if (ex is E.Constant cv)
-        {
-            return cv.Value;
-        }
-        else if (ex is E.Parameter { Name: var id })
-        {
-            return map[id];
-        }
-        else if (ex is E.IntrinsicExpression ie)
-        {
-            if (member.TryGetIntrinsicValue(ie.Kind, ie.Level, ie.ParameterIndex, out var iv))
-                return iv;
-
-            return null; // else error?
-        }
-        else if (ex is E.InterpolatedString i)
-        {
-            var s = new StringBuilder();
-            foreach (var c in i.Contents)
-            {
-                if (c is E.InterpolatedString.StringText text)
-                {
-                    s.Append(text.Text);
-                }
-                else if (c is E.InterpolatedString.Interpolation interpolation)
-                {
-                    var value = Evaluate(interpolation.Expression, member, map);
-
-                    var formatString = (interpolation.Alignment, interpolation.Format) switch
-                    {
-                        ({ } x, null) => $"{{0,{x}}}",
-                        (null, { } x) => $"{{0:{x}}}",
-                        ({ } x, { } y) => $"{{0,{x}:{y}}}",
-                        _ => "{0}",
-                    };
-
-                    s.AppendFormat(map.Culture, formatString, value);
-                }
-            }
-            return s.ToString();
-        }
-        else { return null; } // error? unreachable?
     }
 }
