be1c1e5a7eda656783703d5adcafd19c20925a61
Nobuyuki Iwanaga <ufcpp@live.jp>
2026-03-07 16:49:08 +0900

    Inlines codes directly in top-level program

--- ConstantInterpolation/Examples/Program.cs
+++ ConstantInterpolation/Examples/Program.cs
@@ -1,1 +1,18 @@
-Examples.ConstantInterpolationGeneratorExample.Run();
+using ConstantInterpolationGenerator;
+
+Console.WriteLine($"ab{1}cd{1.2}ef{1.1m}".Invariant());
+Console.WriteLine($"ab{1}cd{1.2}ef{1.1m}".Local("fr"));
+Console.WriteLine($"ab{"constant"}cd".Invariant());
+Console.WriteLine($"/{1234.5,8:n1}/{1234.5,-8:n1}//{1234.5,1:n1}/".Invariant());
+Console.WriteLine($"/{1234.5,8:n1}/{1234.5,-8:n1}//{1234.5,1:n1}/".Local("de"));
+Console.WriteLine($"/{1234.5,8:n1}/{1234.5,-8:n1}//{1234.5,1:n1}/".Local("fr"));
+
+#if false
+Console.WriteLine(1);
+Console.WriteLine($"ab{x}".Invariant());
+Console.WriteLine($"ab{DateTime.Now}".Invariant());
+Console.WriteLine("ja");
+Console.WriteLine($"ab{1}".Local(y));
+Console.WriteLine($"ab{1}".Local("xyz"));
+Console.WriteLine("abc".Local("xyz"));
+#endif
